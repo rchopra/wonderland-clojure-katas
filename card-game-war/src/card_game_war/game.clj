34065cfeab6081270113758297b7cab6da96c3af
Rishi Chopra <rqchopra@gmail.com>
2019-05-01 17:01:46 -0500

    Complete card-game-war kata

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -7,7 +7,31 @@
   (for [suit suits
         rank ranks]
     [suit rank]))
+(def rank-values (zipmap ranks (range (count ranks))))
+(def suit-values (zipmap suits (range (count suits))))
 
-(defn play-round [player1-card player2-card])
+(defn suit [card] (first card))
 
-(defn play-game [player1-cards player2-cards])
+(defn rank [card] (second card))
+
+(defn play-round [player1-card player2-card]
+  (let [rank1 (rank-values (rank player1-card))
+        rank2 (rank-values (rank player2-card))
+        suit1 (suit-values (suit player1-card))
+        suit2 (suit-values (suit player2-card))]
+    (cond
+      (> rank1 rank2) [(into () [player1-card player2-card]) ()]
+      (< rank1 rank2) [() (into () [player1-card player2-card])]
+      (> suit1 suit2) [(into () [player1-card player2-card]) ()]
+      (< suit1 suit2) [() (into () [player1-card player2-card])]
+      :else [(into () [player1-card]) (into () [player2-card])])))
+
+(defn play-game [player1-cards player2-cards]
+  (cond
+    (empty? player1-cards) :player2
+    (empty? player2-cards) :player1
+    :else (let [[player1-winnings player2-winnings] (play-round
+                                                     (first player1-cards)
+                                                     (first player2-cards))]
+            (play-game (concat (rest player1-cards) (shuffle player1-winnings))
+                       (concat (rest player2-cards) (shuffle player2-winnings))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,56 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (let [card1 [:spade 8]
+          card2 [:heart 3]]
+    (is (= [(into () [card1 card2]) ()]
+           (play-round card1 card2)))))
+
+  (testing "queens are higher rank than jacks"
+    (let [card1 [:club :jack]
+          card2 [:club :queen]]
+    (is (= [() (into () [card1 card2])]
+           (play-round card1 card2)))))
+
+  (testing "kings are higher rank than queens"
+    (let [card1 [:club :king]
+          card2 [:club :queen]]
+    (is (= [(into () [card1 card2]) ()]
+           (play-round card1 card2)))))
+
+  (testing "aces are higher rank than kings"
+    (let [card1 [:club :jack]
+          card2 [:diamond :ace]]
+    (is (= [() (into () [card1 card2])]
+           (play-round card1 card2)))))
+
+  (testing "if the ranks are equal, clubs beat spades"
+    (let [card1 [:spade 3]
+          card2 [:club 3]]
+    (is (= [() (into () [card1 card2])]
+           (play-round card1 card2)))))
+
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (let [card1 [:diamond :king]
+          card2 [:club :king]]
+    (is (= [(into () [card1 card2]) ()]
+           (play-round card1 card2)))))
+
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (let [card1 [:diamond 8]
+          card2 [:heart 8]]
+    (is (= [() (into () [card1 card2])]
+           (play-round card1 card2))))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (let [player1-cards cards
+          player2-cards ()]
+    (is (= :player1
+           (play-game player1-cards player2-cards)))))
 
+  (testing "the player who has the ace of hearts wins"
+    (let [player1-cards (take 51 cards)
+          player2-cards (take-last 1 cards)]
+    (is (= :player2
+           (play-game player1-cards player2-cards))))))
